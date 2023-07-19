a9141d200d051939f9caf4d01f1fb7a7e5a5f402
yara <uraeel@gmail.com>
2023-07-19 14:03:34 -0400

    remove unne & added App.test.js

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,9 @@
+import render from "@testing-library/react";
+import App from "../App.js";
+
+describe("<App /> component", () => {
+  test("renders list of events", () => {
+    const { container } = render(<App />).container.firstChild;
+    expect(container.querySelector("#event-list")).toBeInTheDocument();
+  });
+});

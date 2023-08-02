135ed8712ae13832d5853dde327feb065a3449c5
Yara <uraeel@gmail.com>
2023-08-02 14:47:52 -0400

    returned End To End file

--- src/components/__tests__/EndToEnd.test.js
+++ src/components/__tests__/EndToEnd.test.js
@@ -1,41 +1,3 @@
-
-// import puppeteer from 'puppeteer';
-
-// describe('show/hide event details', () => {
-//   let browser;
-//   let page;
-//   beforeAll(async () => {
-//     browser = await puppeteer.launch({
-//       headless: false,
-//       slowMo: 250,
-//       timeout: 0 
-//     });
-//     page = await browser.newPage();
-//     await page.goto('http://localhost:3000/');
-//     await page.waitForSelector('.event');
-//   });
-
-//   afterAll(() => {
-//     browser.close();
-//   });
-
-//   test("An event element is collapsed by default", async () => {
-//     const eventDetails = await page.$(".event .details");
-//     expect(eventDetails).toBeNull();
-//   });
-
-//   test("User can expand an event to see details", async () => {
-//     await page.click(".event .details-btn");
-//     const eventDetails = await page.$(".event .details");
-//     expect(eventDetails).toBeDefined();
-//   });
-
-//   test("User can collapse an event to hide details", async () => {
-//     await page.click(".event .details-btn");
-//     const eventDetails = await page.$(".event .details");
-//     expect(eventDetails).toBeNull();
-//   });
-// });
 import puppeteer from 'puppeteer';
 
 describe('show/hide event details', () => {
@@ -44,8 +6,8 @@ describe('show/hide event details', () => {
   beforeAll(async () => {
     browser = await puppeteer.launch({
       headless: false,
-      slowMo: 250, // slow down by 250ms,
-      timeout: 0 // removes any puppeteer/browser timeout limitations (this isn't the same as the timeout of jest)
+      slowMo: 250,
+      timeout: 0
     });
     page = await browser.newPage();
     await page.goto('http://localhost:3000/');

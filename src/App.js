f0eb53a5365ac3100dc8704db3b12978a313c7c7
Yara <uraeel@gmail.com>
2023-08-10 10:06:55 -0400

    App.js: import InfoAlert:  Fixed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import EventList from "./components/EventList";
 import NumberOfEvents from "./components/NumberOfEvents";
 import { useEffect, useState } from "react";
 import { extractLocations, getEvents } from "./api";
-import InfoAlert, { ErrorAlert } from "./components/Alert";
+import { InfoAlert, ErrorAlert } from "./components/Alert";
 import "./App.css";
 
 const App = () => {
